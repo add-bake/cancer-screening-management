564206e693fe723514059556b9d84a0823b7538b
aimergenge <onlylovesnow14@gmail.com>
2018-10-12 10:57:11 +0800

    style and animate add

--- src/component/IconNumber.vue
+++ src/component/IconNumber.vue
@@ -1,17 +1,47 @@
 <template>
   <div class="wrapper" :style="{'background-color': color}">
     <div class="icon-wrapper">
-      <icon-svg class="pad" icon-class="pad"></icon-svg>
+      <icon-svg class="pad" icon-class="pad" :style="{color: tint(color, 0.3)}"></icon-svg>
       <icon-svg class="on-top" :icon-class="icon"></icon-svg>
+    </div>
+    <div class="text-wrapper">
+      <div class="number">{{animatedNumber}}</div>
+      <div class="desc">{{desc}}</div>
     </div>
   </div>
 </template>
 
 <script>
+import Color from 'color'
+import TweenLite from 'gsap/TweenLite'
+
 export default {
   props: {
     color: String,
-    icon: String
+    icon: String,
+    number: Number,
+    desc: String
+  },
+  data () {
+    return {
+      tweenedNumber: this.number
+    }
+  },
+  computed: {
+    animatedNumber () {
+      return Math.round(this.tweenedNumber)
+    }
+  },
+  watch: {
+    number (newValue) {
+      TweenLite.to(this.$data, 1, {tweenedNumber: newValue})
+    }
+  },
+  methods: {
+    tint (color, percent) {
+      let c = Color(color)
+      return c.mix(Color('white'), 0.3).rgb()
+    }
   }
 }
 </script>
@@ -20,6 +50,9 @@ export default {
 .wrapper {
   border-radius: 8px;
   padding: 25px 30px;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 2px 6px 1px rgba(0, 0, 0, 0.1);
 }
 .icon-wrapper {
   width: 70px;
@@ -27,14 +60,13 @@ export default {
   position: relative;
 }
 .pad {
-  color: hsla(0, 100%, 100%, 0.5);
   font-size: 70px;
   position: absolute;
   top: 0;
   right: 0;
   bottom: 0;
   left: 0;
-  filter: drop-shadow(0 3px 2px rgba(0, 0, 0, 0.2));
+  filter: drop-shadow(0 3px 2px rgba(0, 0, 0, 0.1));
 }
 .on-top {
   color: #fff;
@@ -42,5 +74,23 @@ export default {
   position: absolute;
   top: 15px;
   left: 15px;
+}
+.text-wrapper {
+  padding-left: 14px;
+  color: #fff;
+  align-items: center;
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+}
+.text-wrapper .number {
+  font-family: 'Univers LT 59 Ultra Condensed';
+  font-size: 50px;
+  white-space: nowrap;
+  margin-top: -8px;
+}
+.text-wrapper .desc {
+  font-size: 14px;
+  white-space: nowrap;
 }
 </style>
--- src/page/AppIndex.vue
+++ src/page/AppIndex.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="booking-management">
     <div class="wrap-icons">
-      <IconNumber color="#09befe" icon="index-top-1"></IconNumber>
-      <!-- <IconNumber></IconNumber>
-      <IconNumber></IconNumber>
-      <IconNumber></IconNumber> -->
+      <IconNumber color="#09befe" icon="index-top-1" :number="numbers.one" desc="10天内推广数"></IconNumber>
+      <IconNumber color="#a18dd0" icon="index-top-2" :number="numbers.two" desc="10天内预约数"></IconNumber>
+      <IconNumber color="#e5a43d" icon="index-top-3" :number="numbers.three" desc="未完成体检数"></IconNumber>
+      <IconNumber color="#f56c6c" icon="index-top-4" :number="numbers.four" desc="30天内日均体检数"></IconNumber>
     </div>
   </div>
 </template>
@@ -18,12 +18,22 @@ export default {
   components: {
     IconNumber
   },
-  created() {
-
+  mounted () {
+    this.numbers = {
+      one: 200,
+      two: 50,
+      three: 20,
+      four: 10
+    }
   },
-  data() {
+  data () {
     return {
-
+      numbers: {
+        one: 0,
+        two: 0,
+        three: 0,
+        four: 0
+      }
     }
   },
   methods: {
